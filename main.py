04b6f88df2abe8b9a9bf69c17da02141fcf08d7b
Drei Abmab <dreiabmab@gmail.com>
2026-02-03 15:29:18 +0800

    implemented Terminal UI

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+from modules.Rot_Cryp import ROT32Cipher
+from modules.Vige_Cryp import VigenereCipher
+
 def run():
-    pass
+    rot = ROT32Cipher()
+    vig = VigenereCipher()
+
+    mode = input(
+        "Choose mode (1-ROT32 Encrypt, 2-ROT32 Decrypt, "
+        "3-Vigenere Encrypt, 4-Vigenere Decrypt): "
+    )
+
+    if mode == "1":
+        m = input("Enter a plaintext: ")
+        print("Encrypted Text:", rot.encrypt(m))
+
+    elif mode == "2":
+        c = input("Enter a ciphertext: ")
+        print("Decrypted Text:", rot.decrypt(c))
+
+    elif mode == "3":
+        m = input("Enter a plaintext: ")
+        k = input("Enter a key: ")
+        print("Encrypted Text:", vig.encrypt(m, k))
+
+    elif mode == "4":
+        c = input("Enter a ciphertext: ")
+        k = input("Enter a key: ")
+        print("Decrypted Text:", vig.decrypt(c, k))
+
+    else:
+        print("Invalid choice")
 
 if __name__ == "__main__":
     run()
+    # pass
